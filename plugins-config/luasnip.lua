560ad2e1bc4d7796688d2a2ffe84e07f6b12eb98
Steven Atkinson <atkinson.steven@gmail.com>
2022-11-01 20:16:53 -0700

    luasnip config

---
+++ plugins-config/luasnip.lua
@@ -0,0 +1,21 @@
+local ls = require "luasnip"
+local types = require "luasnip.util.types"
+
+ls.config.set_config {
+    history = true,
+    updateevent = "TextChanged, TextChangedI",
+    enable_autosnippets = true,
+    ext_opts = {
+        [types.choiceNode] = {
+            active = {
+                virt_text = { { "<-", "Error" } },
+            },
+        },
+    },
+
+
+}
+
+
+
+
