07cfc5daebc8db22ae0393dcf2f51be76b21032a
Steven Atkinson <atkinson.steven@gmail.com>
2022-12-29 12:54:51 -0800

    Switched to onedark colorscheme
    Also put plugins at top of file

--- init.lua
+++ init.lua
@@ -1,3 +1,73 @@
+-- exec "set listchars=tab:\uBB\uBB,trail:\uB7,nbsp:~"
+
+require('plugins').use(
+    {
+        'junegunn/fzf',
+        'junegunn/fzf.vim',
+        'vimwiki/vimwiki',
+        'mbbill/undotree',
+        'justinmk/vim-dirvish',
+        'voldikss/vim-floaterm',
+        'tpope/vim-fugitive',
+
+        { 
+            'nvim-telescope/telescope.nvim',
+            branch = '0.1.x', 
+            requires = { 'nvim-lua/plenary.nvim' }},
+        { 
+            'nvim-telescope/telescope-fzf-native.nvim',
+            run = 'make', 
+            cond = vim.fn.executable 'make' == 1 },
+
+        -- treesitter
+        {
+            'nvim-treesitter/nvim-treesitter',
+            run = function()
+                pcall(require('nvim-treesitter.install').update { with_sync = true })
+            end,
+        },
+
+        {
+            'nvim-treesitter/nvim-treesitter-textobjects',
+            after = 'nvim-treesitter',
+        },
+
+        {
+            'neovim/nvim-lspconfig',
+            requires = {
+                'williamboman/mason.nvim',
+                'williamboman/mason-lspconfig.nvim',
+                'j-hui/fidget.nvim',
+            },
+        },
+        -- completion
+        {
+            'hrsh7th/nvim-cmp',
+            requires = {
+                'hrsh7th/cmp-nvim-lsp',
+                'saadparwaiz1/cmp_luasnip',
+                'L3MON4D3/LuaSnip', -- snippet engine
+                'rafamadriz/friendly-snippets',
+            },
+        },
+
+        'lewis6991/gitsigns.nvim',
+        'mhinz/vim-startify',
+        'tpope/vim-eunuch',
+
+        -- Color themes
+        'morhetz/gruvbox',
+        'mhartington/oceanic-next',
+        'sickill/vim-monokai',
+        'joshdick/onedark.vim',
+    })
+
+
+local keymaps = require('keymaps')
+keymaps.normal( {
+    { "<C-n>", ":Startify<CR>" },
+})
+
 
 local opts = require('options')
 opts.set_options( { 
@@ -19,59 +89,8 @@ opts.set_options( {
 })
 
 opts.set_globals({ mapleader = " " })
-opts.set_vim_options({ "colorscheme gruvbox"})
+opts.set_vim_options({ "colorscheme onedark"})
 
 vim.opt.listchars = {
 }
-
--- exec "set listchars=tab:\uBB\uBB,trail:\uB7,nbsp:~"
-
-require('plugins').use({
-         'junegunn/fzf',
-         'junegunn/fzf.vim',
-         'vimwiki/vimwiki',
-         'justinmk/vim-dirvish',
-         'voldikss/vim-floaterm',
-         'tpope/vim-fugitive',
-
-         { 'nvim-telescope/telescope.nvim',
-           branch = '0.1.x', 
-           requires = { 'nvim-lua/plenary.nvim' }},
-         { 'nvim-telescope/telescope-fzf-native.nvim',
-           run = 'make', 
-           cond = vim.fn.executable 'make' == 1 },
-
-
-	 'neovim/nvim-lspconfig',
-         -- completion
-         'hrsh7th/nvim-cmp',
-         'hrsh7th/cmp-buffer',
-         'hrsh7th/cmp-path',
-         'hrsh7th/cmp-path',
-         'hrsh7th/cmp-nvim-lua',
-         'hrsh7th/cmp-nvim-lsp',
-         'hrsh7th/cmp-cmdline',
-         -- 'saadparwaiz1/cmp_luasnip',
-
-         -- snippets
-         'L3MON4D3/LuaSnip', -- snippet engine
-         'rafamadriz/friendly-snippets',
-
-	 'lewis6991/gitsigns.nvim',
-         'mhinz/vim-startify',
-         'tpope/vim-eunuch',
-
-        -- Color themes
-         'morhetz/gruvbox',
-         'mhartington/oceanic-next',
-         'arcticicestudio/nord-vim',
-         'sickill/vim-monokai'
-     })
-
-
-local keymaps = require('keymaps')
-keymaps.normal( {
-	{ "<C-n>", ":Startify<CR>" },
-})
-
 
