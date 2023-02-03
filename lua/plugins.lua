23d712b77f5fa774666444219761beb958bc4ee3
Steven Atkinson <atkinson.steven@gmail.com>
2023-02-03 12:53:45 -0800

    Cleaned up packer bootstrap

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,27 +1,30 @@
 -- Simple wrapper for Packer to manage my plugins
-local Plugins = {}
-
-local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
-
+local M = {}
 
 -- Configures packer and 'uses' the lists of plugins passed in
-function Plugins.use(plugs)
+function M.use(plugs)
+    local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
+    local bootstrapping = false
+
     if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-        vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
-        return 
+        bootstrapping = true
+        vim.fn.system { 'git', 'clone', '--depth', '1', 
+            'https://github.com/wbthomason/packer.nvim', install_path }
+        vim.cmd('packadd packer.nvim')
     end
-    vim.cmd [[
-    augroup Packer
-    autocmd!
-    autocmd BufWritePost init.lua PackerCompile
-    augroup end
-    ]]
+
     local packer = require('packer')
     packer.init()
     packer.use 'wbthomason/packer.nvim' -- Package manager
     for i = 1, #plugs do
         packer.use(plugs[i])
     end
+
+    if bootstrapping then
+        packer.sync()
+    end
+
+    return bootstrpping
 end
 
-return Plugins
+return M
