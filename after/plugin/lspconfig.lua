66319d64dfba22f208e537e88bdfff925f61152a
Steven Atkinson <atkinson.steven@gmail.com>
2023-02-03 12:56:51 -0800

    adds initial lsp config

---
+++ after/plugin/lspconfig.lua
@@ -0,0 +1,72 @@
+local on_attach = function(_, bufnr)
+    local nmap = function(keys, func, desc)
+        if desc then
+            desc = 'LSP: ' .. desc
+        end
+        vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc})
+    end
+
+    nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+    nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+
+    nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+    nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eference')
+    nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplentation')
+    nmap('<leader>D', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+    nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+    nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+
+
+
+    nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
+    nmap('<leader>wl', function()
+        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, '[W]orkspace [L]ist Folders')
+end
+
+require('mason').setup()
+
+local servers = { 'clangd', 'sumneko_lua' }
+
+require('mason-lspconfig').setup {
+    ensure_installed = servers
+}
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+
+for _,lsp in ipairs(servers) do
+    require('lspconfig')[lsp].setup {
+        on_attach = on_attach,
+        capabilities = capabilities,
+    }
+end
+
+require('fidget').setup()
+
+local runtime_path = vim.split(package.path, ';')
+table.insert(runtime_path, 'lua/*.lua')
+table.insert(runtime_path, 'lua/**/init.lua')
+
+require('lspconfig').sumneko_lua.setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+    settings = {
+        lua = {
+            runtime = {
+                version = 'LuaJIT',
+                path = runtime_path,
+            },
+            diagnostics = {
+                globals = { 'vim' },
+            },
+            workspace = {
+                library = vim.api.nvim_get_runtime_file('', true),
+                checkThirdParty = false,
+            },
+            telemetry = { enable = false },
+        },
+    },
+}
+
+require('lsp.clangd').setup()
