1c46ff9c5ab0c20717a9db9c886fc6086321f981
Steven Atkinson <atkinson.steven@gmail.com>
2024-04-07 12:06:24 -0700

    Changed cmp mappings
    Removed tab and enter mappings for standard vim ones
    
    Also added buffers as a source

--- after/plugin/nvim_cmp.lua
+++ after/plugin/nvim_cmp.lua
@@ -1,6 +1,7 @@
 local cmp = require('cmp')
 local luasnip = require('luasnip')
 
+print("nvim-cmp")
 cmp.setup {
     snippet = {
         expand = function(args)
@@ -8,35 +9,17 @@ cmp.setup {
         end,
     },
     mapping = cmp.mapping.preset.insert {
-        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-n>'] = cmp.mapping.select_next_item(),
+        ['<C-p>'] = cmp.mapping.select_prev_item(),
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-space>'] = cmp.mapping.complete(),
-        ['<CR>'] = cmp.mapping.confirm {
-            behavior = cmp.ConfirmBehavior.Replace,
-            select = true,
-        },
-        ['<Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-                luasnip.expand_or_jump()
-            else
-                fallback()
-            end
-        end, {'i', 's'}),
-        ['<S-Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-                luasnip.jump(-1)
-            else
-                fallback()
-            end
-        end, {'i', 's'}),
+        ['<C-y>'] = cmp.mapping.confirm { select = true },
+        ['<C-Space>'] = cmp.mapping.complete(),
     },
     sources = {
         { name = 'nvim_lsp' },
         { name = 'luasnip' },
+        { name = "buffer"},
     },
 }
 
