bb653ee04058ecd5a36db512fed36d2f9744d82e
Steven Atkinson <atkinson.steven@gmail.com>
2024-04-07 15:36:17 -0700

    Cleaned up init.lua
    Rearranaged sections

--- init.lua
+++ init.lua
@@ -2,53 +2,12 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
-require('plugins').use(
-    {
-        -- simple plugins with default config
-        'junegunn/fzf',
-        'junegunn/fzf.vim',
-        'vimwiki/vimwiki',
-        'mbbill/undotree',
-        'nvim-lualine/lualine.nvim',
-        'justinmk/vim-dirvish',
-        'voldikss/vim-floaterm',
-        'tpope/vim-fugitive',
-        'tpope/vim-eunuch',
-        'lewis6991/gitsigns.nvim',
-        'mhinz/vim-startify',
-        -- Color themes
-        'morhetz/gruvbox',
-        'mhartington/oceanic-next',
-        'sickill/vim-monokai',
-        'joshdick/onedark.vim',
-
-        -- plugins with configurtion are in their own files
-        require 'plugins.which-key',
-        require 'plugins.nvim-comment',
-        require 'plugins.telescope',
-        require 'plugins.treesitter',
-        require 'plugins.lsp',
-        require 'plugins.nvim-cmp',
-    })
-
-require('keymaps').normal({
-    { "<C-n>", ":Startify<CR>" },
-    {'<F1>', '<Nop>'},
-    {'<C-h>', '<C-w><C-h>', {desc = 'Move focus to the left window'}},
-    {'<C-l>', '<C-w><C-l>', {desc = 'Move focus to the right window'}},
-    {'<C-j>', '<C-w><C-j>', {desc = 'Move focus to the lower window'}},
-    {'<C-k>', '<C-w><C-k>', {desc = 'Move focus to the upperr window'}},
-    { '<Esc>', '<cmd>nohlsearch<CR>', {desc = 'Clear search highlight'}},
-})
-
-
-local opts = require('options')
-opts.set_options( {
+ require('options').set_options( {
     autoindent = true,
     backup = false,
     breakindent = true,
     clipboard = 'unnamedplus',
-    completeopt=menu,menuone,noselect,
+    completeopt='menu,menuone,noselect',
     expandtab = true,
     hidden = true,
     hlsearch = true,
@@ -72,14 +31,53 @@ opts.set_options( {
 })
 
 
-opts.set_vim_options({ "colorscheme onedark"})
--- vim.keymap.set('n', '<F1>', '<Nop>')
-vim.keymap.set('i', '<F1>', '<ESC>')
+require('keymaps').normal({
+    { "<C-n>", ":Startify<CR>" },
+    {'<F1>', '<Nop>'},
+    {'<C-h>', '<C-w><C-h>', {desc = 'Move focus to the left window'}},
+    {'<C-l>', '<C-w><C-l>', {desc = 'Move focus to the right window'}},
+    {'<C-j>', '<C-w><C-j>', {desc = 'Move focus to the lower window'}},
+    {'<C-k>', '<C-w><C-k>', {desc = 'Move focus to the upperr window'}},
+    { '<Esc>', '<cmd>nohlsearch<CR>', {desc = 'Clear search highlight'}},
+})
+
+-- TODO: how to add these above
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
 
+vim.keymap.set('i', '<F1>', '<ESC>')
 
+
+require('plugins').use( {
+    -- simple plugins with default config
+    'junegunn/fzf',
+    'junegunn/fzf.vim',
+    'vimwiki/vimwiki',
+    'mbbill/undotree',
+    'nvim-lualine/lualine.nvim',
+    'justinmk/vim-dirvish',
+    'voldikss/vim-floaterm',
+    'tpope/vim-fugitive',
+    'tpope/vim-eunuch',
+    'lewis6991/gitsigns.nvim',
+    'mhinz/vim-startify',
+    -- Color themes
+    'morhetz/gruvbox',
+    'mhartington/oceanic-next',
+    'sickill/vim-monokai',
+    'joshdick/onedark.vim',
+
+    -- plugins with configurtion are in their own files
+    require 'plugins.which-key',
+    require 'plugins.nvim-comment',
+    require 'plugins.telescope',
+    require 'plugins.treesitter',
+    require 'plugins.lsp',
+    require 'plugins.nvim-cmp',
+})
+
+-- TODO: where to put this?
 vim.api.nvim_create_autocmd('TextYankPost', {
     desc = 'Hight when yanking (copying) text',
     group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
@@ -87,3 +85,4 @@ vim.api.nvim_create_autocmd('TextYankPost', {
         vim.highlight.on_yank()
     end,
 })
+
