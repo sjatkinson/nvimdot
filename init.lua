7518323ee24dfce75837a16665891f0f19875b95
Steven Atkinson <atkinson.steven@gmail.com>
2024-04-06 18:38:32 -0700

    Modifed Telescope settings

--- init.lua
+++ init.lua
@@ -20,17 +20,30 @@ require('plugins').use(
                 vim.o.timeoutlen = 500
             end,
         },
-        { 
+        {
             'numToStr/Comment.nvim', opts = {}
         },
         {
             'nvim-telescope/telescope.nvim',
             branch = '0.1.x',
-            dependencies = { 'nvim-lua/plenary.nvim' }},
+            dependencies = {
+                'nvim-lua/plenary.nvim',
+                { 'nvim-telescope/telescope-ui-select.nvim' },
+
+                -- Useful for getting pretty icons, but requires a Nerd Font.
+                { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+
+            }
+        },
         {
             'nvim-telescope/telescope-fzf-native.nvim',
             build = 'make',
-            cond = vim.fn.executable 'make' == 1 },
+            cond = vim.fn.executable 'make' == 1
+        },
+        {
+            "nvim-telescope/telescope-file-browser.nvim",
+            dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
+        },
 
         -- treesitter
         {
@@ -43,10 +56,6 @@ require('plugins').use(
         {
             'nvim-treesitter/nvim-treesitter-textobjects',
             dependencies = {'nvim-treesitter'},
-        },
-        {
-            "nvim-telescope/telescope-file-browser.nvim",
-            dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
         },
         {
             'neovim/nvim-lspconfig',
