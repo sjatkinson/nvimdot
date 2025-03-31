74663f10423f1d59cc7740c21f19d59a9fb0b8f4
sjatkinson <atkinson.steven@gmail.com>
2025-03-31 08:50:41 -0700

    Adds key-mapping for which-key
    For local buffer key maps

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,11 +1,19 @@
 return {
     {
         'folke/which-key.nvim',
-        event = 'VimEnter',
-        init = function()
-            vim.o.timeout = true
-            vim.o.timeoutlen = 500
-        end,
+        event = 'VeryLazy',
+        opts = {
+            delay = 800,
+            preset = "modern",
+        },
+        keys = {
+            {
+                "<leader>?",
+                function()
+                    require("which-key").show({ global = false })
+                end,
+                desc = "Buffer Local Keymaps (which-key)",
+            },
+        },
     },
 }
-
